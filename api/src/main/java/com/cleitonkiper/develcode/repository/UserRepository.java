7c996a4f8d3bed9327e7736436457b398439d315
Cleiton Dione Ahnerth Kiper <cleitonahnerth@gmail.com>
2021-07-10 14:45:42 -0400

    create api project

---
+++ api/src/main/java/com/cleitonkiper/develcode/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.cleitonkiper.develcode.repository;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class UserRepository {
+
+}
