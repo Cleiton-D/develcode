67ebe80f9e3ce4a5177017fdb320f0a66cb0d56a
Cleiton Dione Ahnerth Kiper <cleitonahnerth@gmail.com>
2021-07-11 21:39:43 -0400

    add user modal

--- client/src/pages/Dashboard/index.tsx
+++ client/src/pages/Dashboard/index.tsx
@@ -1,44 +1,56 @@
+import { useRef } from 'react';
+
+import UserModal, { UserModalRef } from 'components/UserModal';
+
 import { useListUsers } from 'requests/queries/users';
 
 import * as S from './styles';
 
 const Dashboard = () => {
-  const { data: users } = useListUsers();
+  const modalRef = useRef<UserModalRef>(null);
+
+  const { data: users, key } = useListUsers();
 
   return (
-    <S.Wrapper>
-      <S.PageTitle>Usuários</S.PageTitle>
-      <S.Content>
-        {users && users.length > 0 ? (
-          <table>
-            <S.TableHeader>
-              <tr>
-                <th>Imagem</th>
-                <th>Nome</th>
-                <th>Código</th>
-                <th>Data de Nascimento</th>
-                <th>Ações</th>
-              </tr>
-            </S.TableHeader>
-            <tbody>
-              {users.map((user) => (
-                <tr key={user.id}>
-                  <S.TableCell noPadding>
-                    <S.UserImage imageSrc={user.imagePath} />
-                  </S.TableCell>
-                  <S.TableCell>{user.name}</S.TableCell>
-                  <S.TableCell>{user.code}</S.TableCell>
-                  <S.TableCell>{user.formattedBirthDate}</S.TableCell>
-                  <S.TableCell></S.TableCell>
+    <>
+      <S.Wrapper>
+        <S.PageTitle>Usuários</S.PageTitle>
+        <button type="button" onClick={() => modalRef.current?.openModal()}>
+          adicionar
+        </button>
+        <S.Content>
+          {users && users.length > 0 ? (
+            <table>
+              <S.TableHeader>
+                <tr>
+                  <th>Imagem</th>
+                  <th>Nome</th>
+                  <th>Código</th>
+                  <th>Data de Nascimento</th>
+                  <th>Ações</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        ) : (
-          <S.Message>Nenhum usuário encontrado</S.Message>
-        )}
-      </S.Content>
-    </S.Wrapper>
+              </S.TableHeader>
+              <tbody>
+                {users.map((user) => (
+                  <tr key={user.id}>
+                    <S.TableCell noPadding>
+                      <S.UserImage imageSrc={user.imagePath} />
+                    </S.TableCell>
+                    <S.TableCell>{user.name}</S.TableCell>
+                    <S.TableCell>{user.code}</S.TableCell>
+                    <S.TableCell>{user.formattedBirthDate}</S.TableCell>
+                    <S.TableCell></S.TableCell>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          ) : (
+            <S.Message>Nenhum usuário encontrado</S.Message>
+          )}
+        </S.Content>
+      </S.Wrapper>
+      <UserModal ref={modalRef} queries={[key]} />
+    </>
   );
 };
 
