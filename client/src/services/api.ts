4796072b4994f3f3a7afa5de8a870403a3d05209
Cleiton Dione Ahnerth Kiper <cleitonahnerth@gmail.com>
2021-07-11 21:20:02 -0400

    create useMutation wrapper

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,9 +1,57 @@
 import axios from 'axios';
-import { QueryClient } from 'react-query';
+import {
+  QueryClient,
+  useMutation as useReactQueryMutation,
+  MutationFunction,
+  useQueryClient
+} from 'react-query';
+import { toast, ToastContent } from 'react-toastify';
 
 const api = axios.create({
   baseURL: 'http://127.0.0.1:8080'
 });
+
+type UseMutationOptions = {
+  linkedQueries?: string[];
+  renderError?: (data: any) => ToastContent;
+  renderSuccess?: (data: any) => ToastContent;
+};
+
+export function useMutation(
+  key: string,
+  mutationFn: MutationFunction<any, any>,
+  options: UseMutationOptions = {}
+) {
+  const queryClient = useQueryClient();
+
+  return useReactQueryMutation(key, mutationFn, {
+    onError: (err, data) => {
+      if (options.renderError) {
+        toast({
+          type: toast.TYPE.ERROR,
+          render: options.renderError(data),
+          autoClose: 3000
+        });
+      }
+    },
+    onSuccess: (_, data) => {
+      if (options.renderSuccess) {
+        toast({
+          type: toast.TYPE.SUCCESS,
+          render: options.renderSuccess(data),
+          autoClose: 3000
+        });
+      }
+    },
+    onSettled: () => {
+      if (options.linkedQueries) {
+        options.linkedQueries.map((query) =>
+          queryClient.invalidateQueries(query)
+        );
+      }
+    }
+  });
+}
 
 const queryClient = new QueryClient({
   defaultOptions: {
